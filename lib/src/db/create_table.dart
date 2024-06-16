d44df4a92713f437eb4cdef130bea0ca540eef23
Veroni <zoubheir@gmail.com>
2024-06-16 23:08:29 +0200

    Move sql file creator from orm.dart to create_table.dart

--- lib/src/db/create_table.dart
+++ lib/src/db/create_table.dart
@@ -1,28 +1,46 @@
+import 'package:dartshine/src/orm/types.dart';
 import 'package:sqlite3/sqlite3.dart';
 
-enum DbType{
-  sqlite,
-  postgresql
-}
+enum DbType { sqlite, postgresql }
 
-class CreateTable{
+class CreateTable {
   DbType dbType;
   Database? sqliteDb;
   Map<String, String> dbConfig;
 
-  CreateTable({required this.dbType, required this.dbConfig}){
-    if(dbType == DbType.sqlite){
+  CreateTable({required this.dbType, required this.dbConfig}) {
+    if (dbType == DbType.sqlite) {
       sqliteDb = sqlite3.open(dbConfig['filename']!);
     }
   }
 
-  void create(List<String> tables){
+  void create(List<String> tables) {}
 
-  }
+  void createSqlite(List<Map<String, dynamic>> fields, String tableName) {
+    StringBuffer createQuery = StringBuffer();
 
-  void createSqlite(List<String> tables){
-    for(String table in tables){
-      sqliteDb?.execute(table);
+    createQuery.write('CREATE TABLE IF NOT EXISTS $tableName (');
+
+    for (Map<String, dynamic> field in fields) {
+      if (field['field_name'] is String) {
+        createQuery.write("${field['field_name']} ");
+      }
+
+      if (field['type'] is OrmTypes) {
+        createQuery.write("${ormTypeToString(field['type'])} ");
+      }
+
+      if (field['primary_key'] == true) {
+        createQuery.write('PRIMARY KEY ');
+      }
+
+      if (field['autoincrement'] == true) {
+        createQuery.write('AUTOINCREMENT ');
+      }
+
+      createQuery.write(',');
     }
+
+    createQuery.write(');');
   }
 }
