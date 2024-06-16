af9f3a945269027d0c0d1627187de34ed517a21f
Veroni <zoubheir@gmail.com>
2024-06-16 22:51:04 +0200

    Add sql file creator

--- lib/src/orm/orm.dart
+++ lib/src/orm/orm.dart
@@ -1,8 +1,35 @@
+import 'package:dartshine/src/orm/types.dart';
+
 class Orm{
   String tableName;
   List<Map<String, dynamic>> fields;
+  final StringBuffer createQuery = StringBuffer();
 
-  Orm({required this.tableName, required this.fields});
+  Orm({required this.tableName, required this.fields}){
+    createQuery.write('CREATE TABLE IF NOT EXISTS $tableName (');
+  }
 
-  void migrate(){}
+  void migrate(){
+    for(Map<String, dynamic> field in fields){
+      if(field['field_name'] is String){
+        createQuery.write("${field['field_name']} ");
+      }
+
+      if(field['type'] is OrmTypes){
+        createQuery.write("${ormTypeToString(field['type'])} ");
+      }
+
+      if(field['primary_key'] == true){
+        createQuery.write('PRIMARY KEY ');
+      }
+
+      if(field['autoincrement'] == true){
+        createQuery.write('AUTOINCREMENT ');
+      }
+
+      createQuery.write(',');
+    }
+
+    createQuery.write(');');
+  }
 }
---
+++ lib/src/orm/types.dart
@@ -0,0 +1,15 @@
+enum OrmTypes{
+  int,
+  string,
+}
+
+String ormTypeToString(OrmTypes type){
+  switch (type) {
+    case OrmTypes.int:
+      return 'INTEGER';
+    case OrmTypes.string:
+      return 'TEXT';
+    default:
+    return 'none';
+  }
+}
