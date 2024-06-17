5018b1727e7abae543cd8227b0c77594ef5ba6be
Veroni <zoubheir@gmail.com>
2024-06-17 22:36:47 +0200

    Update orm for pool connection

---
+++ lib/src/orm/db_type.dart
@@ -0,0 +1,4 @@
+enum DbType{
+  sqlite,
+  postgresql
+}
--- lib/src/orm/orm.dart
+++ lib/src/orm/orm.dart
@@ -1,6 +1,52 @@
+import 'package:dartshine/src/orm/db_type.dart';
+import 'package:dartshine/src/orm/types.dart';
+import 'package:sqlite3/sqlite3.dart';
+
 class Orm{
   String tableName;
   List<Map<String, dynamic>> fields;
+  DbType dbType = DbType.sqlite;
+  Database? sqliteDb;
 
-  Orm({required this.tableName, required this.fields});
+  Orm({required this.tableName, required this.fields, required this.dbType, this.sqliteDb});
+
+  void createTable() {
+    if(dbType == DbType.sqlite){
+        createSqliteTable();
+    }
+  }
+
+  void createSqliteTable() {
+    StringBuffer createQuery = StringBuffer();
+
+    createQuery.write('CREATE TABLE IF NOT EXISTS $tableName (');
+
+    for (int i = 0; i < fields.length; i++) {
+      Map<String, dynamic> field = fields[i];
+
+      if (field['field_name'] is String) {
+        createQuery.write("${field['field_name']} ");
+      }
+
+      if (field['type'] is OrmTypes) {
+        createQuery.write("${ormTypeToString(field['type'])} ");
+      }
+
+      if (field['primary_key'] == true) {
+        createQuery.write('PRIMARY KEY ');
+      }
+
+      if (field['autoincrement'] == true) {
+        createQuery.write('AUTOINCREMENT ');
+      }
+
+      if(i != fields.length-1){
+        createQuery.write(',');
+      }
+    }
+
+    createQuery.write(');');
+
+    sqliteDb?.execute(createQuery.toString());
+  }
 }
