b6b1c4a3e2af9fe58fe1b92a8387568d9ad39b74
Veroni <zoubheir@gmail.com>
2024-06-18 00:16:18 +0200

    Add insert for orm

--- lib/src/orm/orm.dart
+++ lib/src/orm/orm.dart
@@ -55,4 +55,38 @@ class Orm{
 
     sqliteDb?.execute(createQuery.toString());
   }
+
+
+  void insert({required Map<String, dynamic> datas}){
+    StringBuffer columnInsertQuery = StringBuffer();
+    StringBuffer valueInsertQuery = StringBuffer();
+
+    columnInsertQuery.write('INSERT INTO $tableName (');
+    valueInsertQuery.write('VALUES (');
+
+    List<MapEntry<String, dynamic>> dataToList = datas.entries.toList();
+
+    for(int i = 0; i < datas.length; i++){
+      MapEntry<String, dynamic> data = dataToList[i];
+
+      columnInsertQuery.write(data.key);
+
+      if(data.value is String){
+        valueInsertQuery.write("'${data.value}'");
+      }else if(data.value is int){
+        valueInsertQuery.write(data.value.toString());
+      }
+
+      if(i < datas.length-1 ){
+        columnInsertQuery.write(',');
+        valueInsertQuery.write(',');
+      }
+    }
+
+    String result = '${columnInsertQuery.toString()}) ${valueInsertQuery.toString()});';
+
+    if(dbType == DbType.sqlite){
+      sqliteDb?.execute(result);
+    }
+  }
 }
